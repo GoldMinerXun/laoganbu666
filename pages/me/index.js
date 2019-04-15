d5c26e1b580aa6ee5b0807bcf6c0d461a8ae2396
unknown <unknown@example.com>
2019-04-15 22:50:15 +0800

    Initial Commit

---
+++ pages/me/index.js
@@ -0,0 +1,77 @@
+// pages/aboutme/index.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      tag:["信用满分","好评如潮"],
+      level:"11.lev",
+      signature:"多行不义必自毙",
+      userInfo: {},
+      hasUserInfo: false,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  handleChange: function (){
+    // console.log(detail)
+    wx.navigateTo({
+      url: '../lab/index',
+    })
+  },
+  onLoad: function () {
+    wx.showToast({
+      title: "loadig...",
+      image: "../../images/loading.png",
+      mask: true
+    })
+    wx.setNavigationBarTitle({
+      title: '个人中心',
+    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }, 
+  onReady: function () {
+    wx.hideLoading()
+  }
+})
